da17ed3659da3ca0070146616e822497f70bc01a
Ubyquit <ingsystem.out.print@gmail.com>
2023-03-27 12:38:52 -0500

    error al inciiar el server

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 // Database connection
 mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(config.port))
