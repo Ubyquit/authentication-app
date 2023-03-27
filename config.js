dbb1c83d61ea74168bb8a2c0a6408071855df187
Ubyquit <ingsystem.out.print@gmail.com>
2023-03-27 11:58:18 -0500

    register.ejs y login.ejs

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 
+//const PORT=3000;
+const MONGO_URI="mongodb://localhost:27017/authentication-app";
+//const SECRET=mi-clave-secreta
+
+
 module.exports = {
-  port: process.env.PORT || 3000,
-  mongoURI: process.env.MONGO_URI,
-  secret: process.env.SECRET
+  port: 3000,
+  mongoURI: MONGO_URI,
+  //secret: process.env.SECRET
 };
